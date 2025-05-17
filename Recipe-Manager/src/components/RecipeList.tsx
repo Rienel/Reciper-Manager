3588542bf202544e18e2ad400043888af39b67be
RIenel <renBasil1016@gmail.com>
2025-05-17 23:18:54 +0800

    parsing props

--- Recipe-Manager/src/components/RecipeList.tsx
+++ Recipe-Manager/src/components/RecipeList.tsx
@@ -1,14 +1,28 @@
+import { useState } from "react";
+
 function RecipeList() {
   let category = ["Breakfast", "Lunch", "Dinner", "Dessert"];
-  category = [];
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
       <h1>My Recipes</h1>
       {category.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {category.map((item) => (
-          <li key={item}>{item}</li>
+        {category.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
